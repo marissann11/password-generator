04857230ab0202975a223369e29ae7c02d0175cc
marissann11 <wilhite.marissa@gmail.com>
2021-12-03 12:29:47 -0500

    added pseudocode outline

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,25 @@
 // Assignment code here
 
 
-// Get references to the #generate element
+// Get references to the #generate element // this targets the generate button 
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+function generatePassword() {
+  console.log("button works");
+
+  return "password goes here";
+}
+
+// Write password to the #password input // function that is called by click of generate button // function from "generatePassword"
+// will be stored in Password variable // no generatePassword function is made !!
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  //password will be displayed on password text on screen
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
+// Add event listener to generate button // when generate button is clicked, writePassword function will be called
 generateBtn.addEventListener("click", writePassword);
