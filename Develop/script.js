122f31605c2ad85d924bd9c3bf62a614e2d5bb75
marissann11 <wilhite.marissa@gmail.com>
2021-12-04 17:50:31 -0500

    updated functions and add returnPassword

--- Develop/script.js
+++ Develop/script.js
@@ -3,19 +3,17 @@
 // Get references to the #generate element // this targets the generate button 
 var generateBtn = document.querySelector("#generate");
 
-const upperChar = ['A','B,','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-const lowerChar = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-const specialChar = ['!','@','#,','$','%','^','&','*','?','.',',']
-const numberChar = ['0','1','2','3','4','5','6','7','8','9']
+const upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lowerChar = "abcdefghijklmnopqrstuvwxyz"
+const specialChar = "!@#$%^&*()_-+=/?.,<>';"
+const numberChar = "0123456789"
+//Generate the password
 
-function generateCriteria() {
-  console.log("yay, this button works");
+function generatePassword() {
 
-//Prompt for password LENGTH (8-128)
+  let passLength = parseInt(window.prompt("How many characters (between 8-128) would you like your password to contain"));
 
-let passLength = parseInt(window.prompt("How many characters (between 8-128) would you like your password to contain"));
-
-if (passLength < 8 || passLength > 128) {
+  if (passLength < 8 || passLength > 128 || isNaN(passLength)) {
   alert("Please enter a valid number between 8 and 128");
   return;
 }
@@ -37,28 +35,33 @@ let includeNumbers = confirm("Would you like to include numbers?");
     return;
   }
 
-  var passwordCriteria = {
-    passLength: passLength,
-    upperChar: includeUpper,
-    lowerChar: includeLower,
-    specialChar: includeSpecial,
-    numbersChar: includeNumbers
-  }
-  return passwordCriteria;
+  var returnPassword = "";
+    if (confirm(includeUpper)) {
+      returnPassword += upperChar; "";
+    }
+    if (confirm(includeLower)) {
+      returnPassword += lowerChar; "";
+    }
+    if (confirm(includeSpecial)) {
+      returnPassword += specialChar; "";
+    }
+    if (confirm(includeNumbers)) {
+      returnPassword += numberChar; "";
+    }
 
-}
+  var finalPassword = "";
+    for (var i = 0; i < passLength; i++) {
+      finalPassword += returnPassword.charAt(Math.floor(Math.random() * returnPassword.length));
+    }
 
-//Generate the password
-
-function generatePassword() {
-
-  
+  console.log(finalPassword);
 
 
 //Display the generated password to the page
-  return "password goes here";
+  return finalPassword;
 }
 
+generatePassword ();
 
 // Write password to the #password input // function that is called by click of generate button // function from "generatePassword"
 // will be stored in Password variable // no generatePassword function is made !!
