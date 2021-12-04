96f30861be594e09f71e8a9153d5386791b4705d
marissann11 <wilhite.marissa@gmail.com>
2021-12-04 18:52:14 -0500

    final commit of js file

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,15 @@
 // Assignment code here
 
-// Get references to the #generate element // this targets the generate button 
+// This targets the generate button 
+
 var generateBtn = document.querySelector("#generate");
 
 const upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const lowerChar = "abcdefghijklmnopqrstuvwxyz"
 const specialChar = "!@#$%^&*()_-+=/?.,<>';"
 const numberChar = "0123456789"
-//Generate the password
+
+// Function to generate the password
 
 function generatePassword() {
 
@@ -15,41 +17,45 @@ function generatePassword() {
 
   if (passLength < 8 || passLength > 128 || isNaN(passLength)) {
   alert("Please enter a valid number between 8 and 128");
-  return;
-}
+  return generatePassword ();
+  }
 
-//Prompt user for uppercase, lowercase, special characters, numeric
+// Prompt user for uppercase, lowercase, special characters, numeric
 
-let includeUpper = confirm("Would you like to include uppercase letters?");
+  let includeUpper = confirm("Would you like to include uppercase letters?");
 
-let includeLower = confirm("Would you like to include lowercase letters?");
+  let includeLower = confirm("Would you like to include lowercase letters?");
 
-let includeSpecial = confirm("Would you like to include special characters?");
+  let includeSpecial = confirm("Would you like to include special characters?");
 
-let includeNumbers = confirm("Would you like to include numbers?");
+  let includeNumbers = confirm("Would you like to include numbers?");
 
-//Validate the user's input (make sure length is between 8-28 and that at least one character type is selected)
+// Validate the user's input (make sure length is between 8-28 and that at least one character type is selected)
 
   if (!includeUpper && !includeLower && !includeSpecial && !includeNumbers) {
     alert("Please choose at least one value!")
-    return;
+    return generatePassword();
   }
 
+// Constructs password based on whether or not above prompts were confirmed
+
   var returnPassword = "";
-    if (confirm(includeUpper)) {
+
+    if (includeUpper === true) {
       returnPassword += upperChar; "";
-    }
-    if (confirm(includeLower)) {
+    } 
+    if (includeLower === true) {
       returnPassword += lowerChar; "";
     }
-    if (confirm(includeSpecial)) {
+    if (includeSpecial === true) {
       returnPassword += specialChar; "";
     }
-    if (confirm(includeNumbers)) {
+    if (includeNumbers === true) {
       returnPassword += numberChar; "";
     }
 
   var finalPassword = "";
+
     for (var i = 0; i < passLength; i++) {
       finalPassword += returnPassword.charAt(Math.floor(Math.random() * returnPassword.length));
     }
@@ -58,21 +64,22 @@ let includeNumbers = confirm("Would you like to include numbers?");
 
 
 //Display the generated password to the page
+
   return finalPassword;
 }
 
-generatePassword ();
-
 // Write password to the #password input // function that is called by click of generate button // function from "generatePassword"
-// will be stored in Password variable // no generatePassword function is made !!
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   //password will be displayed on password text on screen
+
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button // when generate button is clicked, writePassword function will be called
+
 generateBtn.addEventListener("click", writePassword);
